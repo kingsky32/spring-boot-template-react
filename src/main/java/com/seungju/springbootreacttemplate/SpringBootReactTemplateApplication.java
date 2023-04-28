403bd5df97d22ff1673c4a5bb3a826e4e9d74908
Seung Ju <kingsky32@gmail.com>
2023-04-28 18:21:30 +0900

    Initial Commit

---
+++ src/main/java/com/seungju/springbootreacttemplate/SpringBootReactTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.seungju.springbootreacttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactTemplateApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootReactTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seungju/springbootreacttemplate/SpringBootReactTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungju.springbootreacttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactTemplateApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+
+}
